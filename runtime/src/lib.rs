269ad05b304a24eb6856e6db30c8ca894eb6dd5d
yuzhouzhouba <chongchongtech@gmail.com>
2021-12-17 20:42:47 +0800

    configure nicks feather

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -949,4 +949,75 @@ impl_runtime_apis! {
 			Ok(batches)
 		}
 	}
+	
+	
+	pub trait Config: frame_system::Config {
+		/// The overarching event type.
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	
+		/// The currency trait.
+		type Currency: ReservableCurrency<Self::AccountId>;
+	
+		/// Reservation fee.
+		#[pallet::constant]
+		type ReservationFee: Get<BalanceOf<Self>>;
+	
+		/// What to do with slashed funds.
+		type Slashed: OnUnbalanced<NegativeImbalanceOf<Self>>;
+	
+		/// The origin which may forcibly set or remove a name. Root can always do this.
+		type ForceOrigin: EnsureOrigin<Self::Origin>;
+	
+		/// The minimum length a name may be.
+		#[pallet::constant]
+		type MinLength: Get<u32>;
+	
+		/// The maximum length a name may be.
+		#[pallet::constant]
+		type MaxLength: Get<u32>;
+	}
+
+	parameter_types! {
+		// The u128 constant value 500 is aliased to a type named ExistentialDeposit.
+		pub const ExistentialDeposit: u128 = 500;
+		// A heuristic that is used for weight estimation.
+		pub const MaxLocks: u32 = 50;
+	}
+
+	parameter_types! {
+		// Choose a fee that incentivizes desireable behavior.
+		pub const NickReservationFee: u128 = 100;
+		pub const MinNickLength: u32 = 8;
+		// Maximum bounds on storage are important to secure your chain.
+		pub const MaxNickLength: u32 = 32;
+	}
+
+	impl pallet_nicks::Config for Runtime {
+		// The Balances pallet implements the ReservableCurrency trait.
+		// `Balances` is defined in `construct_runtime!` macro. See below.
+		// https://docs.substrate.io/rustdocs/latest/pallet_balances/index.html#implementations-2
+		type Currency = Balances;
+	
+		// Use the NickReservationFee from the parameter_types block.
+		type ReservationFee = NickReservationFee;
+	
+		// No action is taken when deposits are forfeited.
+		type Slashed = ();
+	
+		// Configure the FRAME System Root origin as the Nick pallet admin.
+		// https://docs.substrate.io/rustdocs/latest/frame_system/enum.RawOrigin.html#variant.Root
+		type ForceOrigin = frame_system::EnsureRoot<AccountId>;
+	
+		// Use the MinNickLength from the parameter_types block.
+		type MinLength = MinNickLength;
+	
+		// Use the MaxNickLength from the parameter_types block.
+		type MaxLength = MaxNickLength;
+	
+		// The ubiquitous event type.
+		type Event = Event;
+	}
+
+
+
 }
